4f19dcbb7e807975cc4d9bf0d759ef2e9581d683
TatianaAll <tatiana.allery@orange.fr>
2025-04-28 17:22:16 +0200

    feat: backend skeleton

---
+++ backend.resto/src/main/java/com/tatiana/allery/backend/resto/Application.java
@@ -0,0 +1,13 @@
+package com.tatiana.allery.backend.resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
