eb156b4ce5f653f27ebace533ba88044cf5f3829
TatianaAll <tatiana.allery@orange.fr>
2025-04-30 10:20:52 +0200

    feat: creation entity dish

---
+++ backend.resto/src/main/java/com/tatiana/allery/backend/resto/entity/Dish.java
@@ -0,0 +1,55 @@
+package com.tatiana.allery.backend.resto.entity;
+
+import java.util.Locale.Category;
+//booleanUtils avec la dépendance apache commons
+import org.apache.commons.lang3.BooleanUtils;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "dishes")
+public class Dish {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "price")
+  private float price;
+
+  @Column(name = "image")
+  private String image;
+
+  @Column(name = "is_available")
+  private Boolean isAvailable;
+
+  @ManyToOne
+  @JoinColumn(name = "category_id", nullable = true)
+  private Category category;
+
+  public boolean getAvailable() {
+    return BooleanUtils.isTrue(this.isAvailable);
+  }
+
+}
